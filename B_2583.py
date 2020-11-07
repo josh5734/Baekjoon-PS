7bd9322f52895c0eed04be4f04de93a7dbf8fc2d
Jo Sunghyeon <josh5734@naver.com>
2020-11-08 00:11:44 +0900

    Update B_2583.py
    take care of input order
    m, n, k != n, m, k

--- B_2583.py
+++ B_2583.py
@@ -1,3 +1,6 @@
+import sys
+from sys import *
+setrecursionlimit(10 ** 7)
 
 
 def dfs(graph, x, y, result, info):
@@ -21,7 +24,7 @@ def dfs(graph, x, y, result, info):
 
 
 if __name__ == "__main__":
-    n, m, k = map(int, input().split())
+    m, n, k = map(int, input().split())
     graph = [[1 for i in range(n)] for j in range(m)]
 
     # 분리된 공간과 각 공간이 몇칸인지 저장할 변수 생성
@@ -31,14 +34,13 @@ if __name__ == "__main__":
     # 직사각형으로 가려진 부분을 0으로 채우기
     for i in range(k):
         x1, y1, x2, y2 = map(int, input().split())
-        for p in range(x1, x2):
-            for q in range(y1, y2):
+        for p in range(y1, y2):
+            for q in range(x1, x2):
                 graph[p][q] = 0
 
-    print(graph)
     # dfs 수행
-    for i in range(n):
-        for j in range(m):
+    for i in range(m):
+        for j in range(n):
             if dfs(graph, i, j, result, info) == True:
                 result += 1
     # 결과 출력
