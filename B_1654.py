4f9676fe1e462c780e836d710216fd4bfd1bbfdd
Jo Sunghyeon <josh5734@naver.com>
2020-11-13 23:41:50 +0900

    Create B_1654.py

---
+++ B_1654.py
@@ -0,0 +1,32 @@
+import sys
+
+if __name__ == "__main__":
+
+    # 숫자 정보 입력받기
+    k, n = map(int, input().split())
+    # 랜선 길이 정보 입력받기
+    arr = []
+    for i in range(k):
+        arr.append(int(input()))
+
+    start = 1
+    end = max(arr)
+
+    # 이진 탐색 수행
+    result = 0
+    while(start <= end):
+        total = 0
+        mid = (start + end) // 2
+        #print("mid: ", mid)
+        for x in arr:
+            # 각 x에 대해 몇개로 잘릴 수 있는지 total에 합산
+            if x >= mid:
+                total += int(x / mid)
+        # print(total)
+        if total < n:
+            end = mid - 1
+        else:
+            result = mid
+            start = mid + 1
+
+    print(result)
