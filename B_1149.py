8b69004ed8cb713cfb8e01d95423f719038f0609
Jo Sunghyeon <josh5734@naver.com>
2020-11-28 12:36:09 +0900

    Update B_1149.py

--- B_1149.py
+++ B_1149.py
@@ -1,19 +1,27 @@
 if __name__ == "__main__":
-    house = int(input())
-    cost = []
-    for i in range(house):
-        cost.append(list(map(int, input().split())))
+    # 집의 개수
+    number_of_house = int(input())
 
-    dp = [0] * 1000
-    dp[0] = min(cost[0])
-    flag = cost[0].index(dp[0])
+    # 각 집을 R,G,B로 색칠할 때 드는 비용 정보
+    rgb_cost = []
+    for i in range(number_of_house):
+        rgb_cost.append(list(map(int, input().split())))
 
-    for h in range(1, house):
-        temp = []
-        for c in range(3):
-            if c != flag:
-                temp.append(cost[h][c])
-        dp[h] = min(temp)
-        flag = cost[h].index(dp[h])
+    # 각 집을 R,G,B로 칠할 때 드는 비용의 합 누적정보
+    rgb_cost_total = [[0, 0, 0] for i in range(number_of_house)]
 
-    print(dp)
+    # 첫번째 집을 R,G,B로 칠할 때 드는 비용 초기화
+    rgb_cost_total[0] = rgb_cost[0]
+
+    # 각 집의 R,G,B 누적 최소 비용은 (각 색깔로 해당 집을 칠하는 비용) + (나머지 색깔로 그 전 집까지 칠한 최소 누적 비용)
+    for i in range(1, number_of_house):
+        rgb_cost_total[i][0] = rgb_cost[i][0] + \
+            min(rgb_cost_total[i-1][1], rgb_cost_total[i-1][2])
+        rgb_cost_total[i][1] = rgb_cost[i][1] + \
+            min(rgb_cost_total[i-1][0], rgb_cost_total[i-1][2])
+        rgb_cost_total[i][2] = rgb_cost[i][2] + \
+            min(rgb_cost_total[i-1][0], rgb_cost_total[i-1][1])
+
+    # 최소 비용은 rgb_cost_total의 마지막에서 가장 작은 값
+    min_cost = min(rgb_cost_total[-1])
+    print(min_cost)
