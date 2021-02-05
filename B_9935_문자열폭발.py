404a35907603de56eed0a27f8450d1d8efe46dfc
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-02-05 14:22:36 +0900

    Update B_9935_문자열폭발.py

--- B_9935_문자열폭발.py
+++ B_9935_문자열폭발.py
@@ -31,22 +31,3 @@ if __name__ == "__main__":
                     stack.append(w)
     answer = ''.join(stack)
     print("FRULA" if answer == "" else answer)
-    '''
-    # 순회를 하면서 bomb 문자열을 탐색할 cursor
-    cursor = 0
-
-    str_length = len(str)
-    bomb_length = len(bomb)
-    while cursor <= str_length - bomb_length:
-        # bomb 문자열을 발견했을 때
-        if str[cursor:cursor+bomb_length] == bomb:
-            str = str.replace(bomb, "")
-            # 커서가 시작 위치가 아니라면 문자열을 삭제하고 커서를 한 칸 앞으로 당긴다.
-            if cursor == 0:
-                continue
-            else:
-                cursor -= (bomb_length - 1)
-        else:
-            cursor += 1
-    print("FRULA" if str == "" else str)
-    '''
