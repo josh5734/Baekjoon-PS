b88ff447407ba0715c575ef6eaa9851aa439fe46
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-02-08 15:04:22 +0900

    Create B_2437_저울.py

---
+++ B_2437_저울.py
@@ -0,0 +1,34 @@
+import sys
+
+
+if __name__ == "__main__":
+
+    n = int(input())
+    pend = list(map(int, sys.stdin.readline().split()))
+    pend.sort()
+
+    # 측정할 수 있는 무게를 맨 처음 = 0으로 설정
+    weight = [0]
+
+    # 그전까지 측정할 수 있는 무게에서 최대값과 현재 가지고 있는 추의 무게를 비교해서 1보다 차이가 크면 끝남
+    for p in pend:
+        if p - weight[-1] > 1:
+            break
+        else:
+            # 측정할 수 있는 가장 큰 무게만 기록하기
+            weight.append(p+weight[-1])
+
+    # 결과 출력
+    print(weight[-1]+1)
+
+
+'''
+1 1 2 3 6 7 30
+1
+1 2
+1 2 3 4
+1 2 3 4 5 6 7
+1 2 3 4 5 6 7 8 9 10 11 12 13
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+
+'''
