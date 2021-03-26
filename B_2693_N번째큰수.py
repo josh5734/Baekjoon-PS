c746339e4ed22fce2e12e244a5423864ddfb99af
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-03-26 19:08:53 +0900

    Create B_2693_N번째큰수.py

---
+++ B_2693_N번째큰수.py
@@ -0,0 +1,7 @@
+import sys
+if __name__ == "__main__":
+    tc = int(input())
+    for _ in range(tc):
+        numbers = list(map(int, sys.stdin.readline().split())
+                       ).sort(reverse=True)
+        print(numbers[2])
