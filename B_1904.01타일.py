6592c064504e3305580a5865995350cedeeeae90
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-05-07 23:36:58 +0900

    Create B_1904.01타일.py

---
+++ B_1904.01타일.py
@@ -0,0 +1,12 @@
+n = int(input())
+dp = [0] * (n+1)
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
+else:
+    dp[1], dp[2] = 1, 2
+
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 15746 
+    print(dp[n])
