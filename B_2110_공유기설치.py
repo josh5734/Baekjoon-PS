4d5fdf0ef9a5270ea4a1497686f309e8aa5ae3eb
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-05-23 12:15:32 +0900

    Update B_2110_공유기설치.py

--- B_2110_공유기설치.py
+++ B_2110_공유기설치.py
@@ -1,28 +1,26 @@
 import sys
-
 if __name__ == "__main__":
-
-    # 숫자 정보 입력받기
+    answer = 0
     n, c = map(int, input().split())
-    # 집 위치 정보 기록
-    arr = []
-    for i in range(n):
-        arr.append(int(input()))
-    arr.sort()
+    house = []
+    for _ in range(n):
+        house.append(int(sys.stdin.readline().rstrip()))
+    house.sort()  # 원점을 기준으로 정렬
 
-    start, end = arr[0], arr[-1]
-    result = 0
-
-    while(start <= end):
-        total = 0
+    start, end = 0, int(1e10)
+    while start <= end:
+        cursor = house[0]  # 첫번째 집을 기준
         mid = (start + end) // 2
-        for house in arr:
-            if house % mid == 0:
-                total += 1
-        if total < c:
-            end = mid - 1
-        else:
-            result = mid
-            start = mid + 1
+        count = 1
+        # 현재로부터 mid 만큼 떨어진 곳보다 멀리 집이 있다면 거기에 공유기 설치를 해본다.
+        for i in range(1, len(house)):
+            if house[i] >= cursor + mid:
+                count += 1
+                cursor = house[i]
 
-    print(result)
+        if count >= c:  # 모든 집을 돌아본 뒤 공유기가 C보다 많이 설치 되었다면 공유기 사이의 거리를 더 늘려서 가능한 집을 줄여보자.
+            start = mid + 1
+            answer = mid
+        else:
+            end = mid - 1
+    print(answer)
