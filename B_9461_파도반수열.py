25cc9571a52d47e66342105cc64d48e639f8bf51
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-06-04 17:43:45 +0900

    Create B_9461_파도반수열.py

---
+++ B_9461_파도반수열.py
@@ -0,0 +1,9 @@
+tc = int(input())
+dp = [0] * 101
+dp[1], dp[2], dp[3] = 1, 1, 1
+for i in range(4, 101):
+    dp[i] = dp[i-2] + dp[i-3]
+
+for _ in range(tc):
+    n = int(input())
+    print(dp[n])
