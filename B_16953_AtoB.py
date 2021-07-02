792db1446a882ddb14676bb9bbe1e059da5af4dd
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-07-03 00:55:27 +0900

    Update B_16953_AtoB.py

--- B_16953_AtoB.py
+++ B_16953_AtoB.py
@@ -1,3 +1,38 @@
+import sys
+
+source, target= map(int, sys.stdin.readline().split())
+
+if source == target:
+    print(1)
+else:
+    count = 1
+    found = False
+    while target > source:
+        # 현재 타겟값의 맨 뒤가 1이면 무조건 1을 빼봐야 한다.
+        if str(target)[-1] == '1':
+            target = (target - 1) // 10
+            count += 1
+            if target == source:
+                found = True
+                print(count)  
+                break      
+        if target % 2 == 0:
+            target //= 2
+            count += 1
+            if target == source:
+                found = True
+                print(count)
+                break      
+
+        # 현재 타겟값이 홀수인데 마지막도 1이 아니면 불가능해진다.
+        if target % 2 == 1 and str(target)[-1] != '1':
+            break
+    
+    # 불가능하다면 -1 출력
+    if not found:
+        print(-1)
+
+''' 메모리 초과'
 a, b = map(int,input().split())
 answer = 0
 
@@ -25,14 +60,13 @@ else:
                     temp.append(multiple)
                     temp.append(appendOne)
             candidate = list(set(temp))
-
             # 짝수이지만 만들 수 없는 경우
-            if min(candidate) > b:
-                answer = -2
-                break
-
             if flag:
                 answer += 1
                 break
+            if min(candidate) > b:
+                answer = -2
+                break
             answer += 1
 print(answer+1)
+'''
