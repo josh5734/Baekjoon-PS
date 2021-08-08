ac17d2090e82da321b58362357d6c47aec16d2b4
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-08-08 15:54:24 +0900

    Create B_5639_이진 검색 트리.py

---
+++ B_5639_이진 검색 트리.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+def postOrder(start, end):
+    
+    if start > end:
+        return
+    
+    root = preOrder[start]
+    idx = start + 1
+
+    while idx <= end:
+        if preOrder[idx] > root:
+            break
+        idx += 1
+    
+    postOrder(start +1 , idx-1)
+    postOrder(idx, end)
+    print(root)
+
+
+
+
+preOrder = []
+while True:
+    try:
+        preOrder.append(int(sys.stdin.readline()))
+    except:
+        break
+postOrder(0, len(preOrder) - 1)
+
