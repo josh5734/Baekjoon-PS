9b8301d462ae206a09936efd0b5f96fa2843033c
josunghyeon <61929745+josh5734@users.noreply.github.com>
2021-10-21 23:58:35 +0900

    Update B_1253_좋다.py

--- B_1253_좋다.py
+++ B_1253_좋다.py
@@ -1,18 +1,12 @@
 import sys
+from bisect import bisect_left, bisect_right
 input = sys.stdin.readline
 
-def findTarget(numbers, target):
-    start, end = 0, len(numbers)-1
-    while start <= end:
-        mid = (start + end) // 2
-        if numbers[mid] == target: 
-            return mid
-        elif numbers[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return -1      
-
+def findTargetRange(numbers, target, i, j):
+    left = bisect_left(numbers, target)
+    right = bisect_right(numbers, target)
+    return [idx for idx in range(left, right) if idx != i and idx != j]
+    
 answer = 0
 n = int(input())
 numbers = list(map(int, input().split()))
@@ -20,10 +14,12 @@ numbers.sort() # 오름차순 정렬
 
 for i in range(n):
     for j in range(n):
-        # 다른 수 두 개의 합으로 나타내야 함
+        # 다른 두 수의 합으로 나타낸다 -> i, j는 달라야함
+        if i == j: continue
         target = numbers[i] - numbers[j]
-        result = findTarget(numbers, target)
-        if result != -1 and i != j and i != result:
+        # 주어진 배열에서 i, j를 제외하고 target값이 존재하는지 확인
+        result = findTargetRange(numbers, target, i, j)
+        if len(result) >= 1:
             answer += 1
             break
 print(answer)
