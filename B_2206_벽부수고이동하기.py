423e71d8e860ea6ba3dfb0d6535bdead7ae1b2e6
sunghyeon <josh5734@naver.com>
2021-12-12 19:42:18 +0900

    Update B_2206_벽부수고이동하기.py

--- B_2206_벽부수고이동하기.py
+++ B_2206_벽부수고이동하기.py
@@ -1,84 +1,48 @@
 import sys
 from collections import deque
 
-sys.setrecursionlimit(10000)
-
-
-def bfs(x, y):
-    q = deque()
-    q.append((x, y))
-    arrive = False
-    while q:
-        x, y = q.popleft()
-
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-
-            # 이동 가능한 공간을 벗어나면 무시
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                q.append((nx, ny))
-
-        if x == n-1 and y == m-1:
-            arrive = True
-    if not arrive:
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            if graph[nx][ny] == 0:
-                k = bfs2(nx, ny, graph[x][y]+1)
-        return k
-        # print(graph)
-    return graph[n-1][m-1]
-
-
-def bfs2(x, y, now):
-    q = deque()
-    q.append((x, y))
-    while q:
-        x, y = q.popleft()
-
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-
-            # 이동 가능한 공간을 벗어나면 무시
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-
-            if graph[nx][ny] == 1:
-
-                graph[nx][ny] = graph[x][y] + 1
-                q.append((nx, ny))
-        # print(graph)
-    return graph[n-1][m-1] + now
-
-
-if __name__ == "__main__":
-    n, m = map(int, input().split())
-
-    # graph 정보
-    graph = [[] for _ in range(n)]
-    for i in range(n):
-        graph[i] = list(map(int, sys.stdin.readline().rstrip()))
-
-    # 편의를 위해 graph의 0, 1 정보를 바꿔서 생각하자.
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 0:
-                graph[i][j] = 1
-            else:
-                graph[i][j] = 0
-
+def bfs():
     # 이동할 수 있는 방향
     dx = [-1, 1, 0, 0]
     dy = [0, 0, 1, -1]
 
-    sp = bfs(0, 0)
-    # 도착할 수 있다면 그 최단 경로는 항상 n+m-1보다는 크거나 같아야 함
-    if sp < n+m-1:
-        print(-1)
-    else:
-        print(sp)
+    #visited[x][y][breakWall] = (x,y) 지점의 breakWall한 상태에서 거리
+    visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+    visited[0][0][0] = 1
+
+
+    # 큐에 처음 거리 1과 시작점 (0,0), 벽을 부순 횟수(0)을 넣고 시작
+    q = deque()
+    q.append((0,0,0))
+    result = []
+    while q:
+        x, y, breakWall = q.popleft()
+  
+        if x == n-1 and y == m-1:
+            result.append(visited[x][y][breakWall])
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                # 이동할 위치가 벽인 경우 현재 벽을 한 번도 부수지 않았으면 벽을 부수고 이동    
+                if graph[nx][ny] == 1:
+                    if visited[nx][ny][1] == 0 and breakWall == 0:
+                        visited[nx][ny][1] = visited[x][y][0] + 1
+                        q.append((nx, ny, 1))
+                # 벽이 아닌 경우
+                else:
+                    if visited[nx][ny][breakWall] == 0:
+                        visited[nx][ny][breakWall] = visited[x][y][breakWall] + 1
+                        q.append((nx, ny, breakWall))
+
+    return min(result) if result else -1  
+
+
+n, m = map(int, input().split())
+
+# graph 정보
+graph = [[] for _ in range(n)]
+for i in range(n):
+    graph[i] = list(map(int, sys.stdin.readline().rstrip()))
+
+print(bfs())
