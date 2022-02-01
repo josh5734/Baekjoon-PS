c983c92dd502325ab2df6db21ae25545965e8b90
sunghyeon <josh5734@naver.com>
2022-02-01 23:41:50 +0900

    Create B_11062_카드 게임.py

---
+++ B_11062_카드 게임.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+tc = int(input()) # 테스트케이스의 수
+
+for _ in range(tc):
+    n = int(input())
+    cards = list(map(int, input().split()))
+    
+    # 서로 자신의 점수를 가장 높이기 위해 최선의 전략으로 임한다.
+
+  
